148c589bee327ebbb2651e878e58f6ffcaf8352b
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2015-02-02 13:54:31 -0800

    qcacld: Fix Kernel Panic during driver unload.
    Prima to qcacld-2.0 propagation
    
    During driver unload, no protection mechanism
    is provided for sme queue. As a result between
    sme_stop and sme_close there is a possibility
    to queue SME command. As a part of fix, made
    sure that no command enters into queue while
    unload is in progress.
    
    Change-Id: Ieef5b4a9fb89bb0dd5c3462b80f6ed1c2711632a
    CRs-Fixed: 790031
    (cherry picked from commit 975ea4e1f00c8b8eacf2eadcb774f18c8dc34b1f)

--- CORE/SME/src/csr/csrApiRoam.c
+++ CORE/SME/src/csr/csrApiRoam.c
@@ -17744,6 +17744,11 @@ eHalStatus csrQueueSmeCommand( tpAniSirGlobal pMac, tSmeCmd *pCommand, tANI_BOOL
 {
     eHalStatus status;
 
+    if (!SME_IS_START(pMac)) {
+        smsLog(pMac, LOGE, FL("Sme in stop state"));
+        return eHAL_STATUS_FAILURE;
+    }
+
     if( (eSmeCommandScan == pCommand->command) && pMac->scan.fDropScanCmd )
     {
         smsLog(pMac, LOGW, FL(" drop scan (scan reason %d) command"),
