21bede51bd4b28be67f14e07adc40d568259c089
Alex Gonzalez <alex.gonzalez@digi.com>
2018-09-07 12:06:41 +0200

    Adapt to release of private netdev state
    This is from upstream commit
    cf124db566e6b036b8bcbe8decbed740bdfac8c6
    
    Signed-off-by: Alex Gonzalez <alex.gonzalez@digi.com>

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -8359,7 +8359,11 @@ static hdd_adapter_t* hdd_alloc_station_adapter(hdd_context_t *pHddCtx,
          pWlanDev->features |= NETIF_F_RXCSUM;
       hdd_set_station_ops( pAdapter->dev );
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0))
+      pWlanDev->needs_free_netdev = true;
+#else
       pWlanDev->destructor = free_netdev;
+#endif
       pWlanDev->ieee80211_ptr = &pAdapter->wdev ;
       pAdapter->wdev.wiphy = pHddCtx->wiphy;
       pAdapter->wdev.netdev =  pWlanDev;
